49bc201307021e361e5b7cdc83edb9606bbbd309
Riley <rileypeuter@bigpond.com>
2023-02-26 23:21:39 +1100

    Add project files.

---
+++ IFE 0.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFE_0._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
